54c6d6282fb62b2c3342787a9aee29ba1b5adebb
MJ_JUNG <dwrhksfl77@naver.com>
2020-05-16 21:48:27 +0900

    template 고급

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <string>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+
+template<typename TupleType, int n>
+class tuple_print
+{
+	public:
+		tuple_print(const TupleType& t) {
+			tuple_print<TupleType, n - 1>tp(t);
+			cout << get<n - 1>(t) << endl;
+		}
+};
+
+template<typename TupleType>
+class tuple_print<TupleType, 0>
+{
+public:
+	tuple_print(const TupleType&) {}
+};
+
+
+
+int main()
+{
+	
+	using MyTuple = tuple<int, string, bool>;
+	MyTuple t1(16, "fox", true);
+	tuple_print<MyTuple, tuple_size<MyTuple>::value > tp(t1);
+
+
+
+	return 0;
+
+
+}
+
